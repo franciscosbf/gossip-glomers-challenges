d981131773ae48b5d67b81e3dd0125edd982275a
Francisco Braço-Forte <f.s.f.b2001@gmail.com>
2025-02-10 00:23:11 +0000

    nothing special

--- 3-broadcast/workload-b.sh
+++ 3-broadcast/workload-b.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 go build -o broadcast main.go &&
-	maelstrom test -w broadcast --bin broadcast \
+	maelstrom test -w broadcast --bin ./broadcast \
 		--node-count 5 --time-limit 20 --rate 10
